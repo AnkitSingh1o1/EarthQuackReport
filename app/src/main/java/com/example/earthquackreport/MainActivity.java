0993170428d79c9cbf8886a6ef91efa23b01674d
Ankit Singh <ankitsinghniti@gmail.com>
2022-02-28 13:59:55 +0530

    Commit 2: Networking Added to this app. Working good.

--- app/src/main/java/com/example/earthquackreport/MainActivity.java
+++ app/src/main/java/com/example/earthquackreport/MainActivity.java
@@ -4,40 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** Adapter for the list of earthquakes */
+    private QuackReportAdapter mAdapter;
+
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<QuackReport> arr = QueryUtils.extractEarthquakes();
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.listView);
 
-//        arr.add(new QuackReport("7.1'", "Japan", "3:00pm"));
-//        arr.add(new QuackReport("7.1'", "Japan", "3:00pm"));
-//        arr.add(new QuackReport("7.1'", "Japan", "3:00pm"));
-//        arr.add(new QuackReport("7.1'", "Japan", "3:00pm"));
-//        arr.add(new QuackReport("7.1'", "Japan", "3:00pm"));
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new QuackReportAdapter(this, new ArrayList<QuackReport>());
 
-        QuackReportAdapter quackReportAdapter = new QuackReportAdapter(this, arr);
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(mAdapter);
 
-        ListView quackListView = findViewById(R.id.listView);
-
-        // set the numbersViewAdapter for ListView
-        quackListView.setAdapter(quackReportAdapter);
-
-        quackListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake.
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 // Find the current earthquake that was clicked on
-                QuackReport currentEarthquake = quackReportAdapter.getItem(position);
+                QuackReport currentEarthquake = mAdapter.getItem(position);
 
                 // Convert the String URL into a URI object (to pass into the Intent constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
@@ -50,5 +54,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+    }
+
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<QuackReport>> {
+        /**
+         * This method runs on a background thread and performs the network request.
+         * We should not update the UI from a background thread, so we return a list of
+         * {@link QuackReport}s as the result.
+         */
+        @Override
+        protected List<QuackReport> doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<QuackReport> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+        /**
+         * This method runs on the main UI thread after the background work has been
+         * completed. This method receives as input, the return value from the doInBackground()
+         * method. First we clear out the adapter, to get rid of earthquake data from a previous
+         * query to USGS. Then we update the adapter with the new list of earthquakes,
+         * which will trigger the ListView to re-populate its list items.
+         */
+        @Override
+        protected void onPostExecute(List<QuackReport> data) {
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
+
+            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+        }
     }
 }
